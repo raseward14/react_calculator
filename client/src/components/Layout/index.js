9137f812f647164a21a81803d99e80abc36cc214
Richard Seward <raseward14@gmail.com>
2023-11-30 18:35:15 -0700

    styled flexbox return and interface classes

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -4,25 +4,19 @@ import './style.css';
 const Layout = () => {
     return (
         <div className='App'>
-            <table>
-                <thead>
-                    <tr>
-                        <th>
-                            <h1>React Calculator</h1>
-
-                        </th>
-                    </tr>
-                </thead>
-                <tbody className='calc-container'>
-                    <tr >
-                        <th >
-                            <div className='calc-wrapper'>
-                                text
-                            </div>
-                        </th>
-                    </tr>
-                </tbody>
-            </table>
+            <h1>React Calculator</h1>
+                <table className='calc-wrapper'>
+                    <thead className='return'>
+                        <tr className='value'>0
+                        </tr>
+                    </thead>
+                    <tbody className='interface'>
+                        <tr >
+                            <th>text
+                            </th>
+                        </tr>
+                    </tbody>
+                </table>
             <Outlet />
         </div>
     )
