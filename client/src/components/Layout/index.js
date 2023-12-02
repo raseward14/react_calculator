e700da24bcb9f0ba181dd03c6cc3372418ae3c61
richardseward <raseward14@gmail.com>
2023-12-01 22:39:41 -0700

    added columns for buttons and orange operator section

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -2,17 +2,27 @@ import { Outlet } from 'react-router-dom';
 import './style.css';
 
 const Layout = () => {
+
+
     return (
-        <div className='App'>
+        <div  className='App'>
             <h1>React Calculator</h1>
                 <table className='calc-wrapper'>
                     <thead className='return'>
                         <tr className='value'>0
                         </tr>
                     </thead>
-                    <tbody className='interface'>
-                        <tr >
-                            <th>text
+                    <tbody className='interface-container'>
+                        <tr className='interface'>
+                            <th className='left'>
+                            <tr className='c-container'>
+                                <th className='c1'>1</th>
+                                <th className='c2'>2</th>
+                                <th className='c3'>3</th>
+                            </tr>
+                            <tr></tr>
+                            </th>
+                            <th className='right'>right
                             </th>
                         </tr>
                     </tbody>
