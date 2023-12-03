e9488499bfc407b91f4255b896bb043ffb7c1e21
richardseward <raseward14@gmail.com>
2023-12-02 18:13:40 -0700

    flex-grow for left side of cal was changed to 4, so it takes up 3 of four parts

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -5,28 +5,33 @@ const Layout = () => {
 
 
     return (
-        <div  className='App'>
+        <div className='App'>
             <h1>React Calculator</h1>
-                <table className='calc-wrapper'>
-                    <thead className='return'>
-                        <tr className='value'>0
-                        </tr>
-                    </thead>
-                    <tbody className='interface-container'>
-                        <tr className='interface'>
-                            <th className='left'>
+            <table className='calc-wrapper'>
+                <thead className='return'>
+                    <tr className='value'>0
+                    </tr>
+                </thead>
+                <tbody className='interface-container'>
+                    <tr className='interface'>
+                        <th className='left'>
                             <tr className='c-container'>
                                 <th className='c1'>1</th>
                                 <th className='c2'>2</th>
                                 <th className='c3'>3</th>
                             </tr>
                             <tr></tr>
-                            </th>
-                            <th className='right'>right
-                            </th>
-                        </tr>
-                    </tbody>
-                </table>
+                        </th>
+                        <th className='right'>right
+                        <tr>1</tr>
+                        <tr>2</tr>
+                        <tr>3</tr>
+                        <tr>4</tr>
+                        <tr>5</tr>
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
             <Outlet />
         </div>
     )
