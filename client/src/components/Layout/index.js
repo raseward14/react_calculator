0d7933615c5e83a83509f3680fa7eb084bf8b0ec
richardseward <raseward14@gmail.com>
2023-12-04 19:41:59 -0700

    removed unused styles to clean up style.css so every line is needed

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -14,8 +14,8 @@ const Layout = () => {
                 </thead>
                 <tbody className='interface-container'>
                     <tr className='interface'>
-                        <th className='left'>
-                            <tr className='top-row'>
+                        <th className='left-container'>
+                            <tr className='left-side'>
                                 <th className='left-columns'>
                                     <tr className='left-row'>
                                         <td>AC</td>
@@ -29,11 +29,11 @@ const Layout = () => {
                                     <tr className='left-row'>
                                         <td>1</td>
                                     </tr>
-                                    <tr className='left-row left-corner-test'>
+                                    <tr className='left-row left-corner'>
                                         <td>0</td>
                                     </tr>
                                 </th>
-                                <th className='left-columns'>
+                                <th className='left-center-column'>
                                     <tr className='left-center-row'>
                                         <td>+/-</td>
                                     </tr>
@@ -66,30 +66,18 @@ const Layout = () => {
 
                                 </th>
                             </tr>
-                            {/* <tr className='bottom-row'>
-                                <th className='left-row left-corner'>
-                                    <tr>
-                                        <td>0</td>
-                                    </tr>
-                                </th>
-                                <th className='left-row period'>
-                                    <tr>
-                                        <td>.</td>
-                                    </tr>
-                                </th>
-                            </tr> */}
                         </th>
-                        <th className='right'>
-                            <tr className='right-row bold'>
+                        <th className='right-side'>
+                            <tr className='right-row big'>
                                 <td>÷</td>
                             </tr>
-                            <tr className='right-row bold'>
+                            <tr className='right-row big'>
                                 <td>x</td>
                             </tr>
-                            <tr className='right-row bold'>
+                            <tr className='right-row big'>
                                 <td>-</td>
                             </tr>
-                            <tr className='right-row bold'>
+                            <tr className='right-row big'>
                                 <td>+</td>
                             </tr>
                             <tr className='right-row right-corner bold'>
